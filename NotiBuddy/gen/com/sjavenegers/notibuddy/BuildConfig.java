1f6ac6bdf0b658abbb68f41253743b913fe37811
Alben Cheung <alben.cheung@gmail.com>
2013-11-09 00:48:20 -0800

    cleaned

--- NotiBuddy/gen/com/sjavenegers/notibuddy/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.sjavenegers.notibuddy;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-NotiBuddy - Notification Engine
